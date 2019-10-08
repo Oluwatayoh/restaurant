4a94b325dadb3455c24b52a96fd0407777b0aa5c
Oluwatayoh <ojotayoh2@gmail.com>
2019-10-08 16:42:12 +0100

    update

--- src/app/core-modules/core-modules.component.ts
+++ src/app/core-modules/core-modules.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+declare const window: any;
+
 
 @Component({
   selector: 'app-core-modules',
@@ -10,7 +12,18 @@ export class CoreModulesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
+  }
 
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 100) {
+      console.log('You are 100px from the top to bottom');
+    } else if (number > 500) {
+        console.log('You are 500px from the top to bottom');
+    }
   }
 
 }
